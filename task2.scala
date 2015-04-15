61f98c423b5ab640a34632327a1ff3cc01f5683b
Nimrod Lahav <nimrodl@wix.com>
2015-04-16 00:28:38 +0300

    Update task2.scala

--- task2.scala
+++ task2.scala
@@ -16,16 +16,16 @@ class Simple2 extends Simulation {
 		.acceptLanguageHeader("en-US,en;q=0.8,he;q=0.6")
 		.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36")
 
-	val headers_0 = Map("whoAmI" -> "Nimrod")
+	val headers_0 = Map("whoAmI" -> "Nimrod") //just to see who this is in logs/relic
 
 	val scn = scenario("scenario")
 		.exec(http("get home page")
 			.get("/nimrod.php")
-			.headers(headers_0))
-			.pause(1 seconds, 5 seconds)
+			.headers(headers_0)) //adds the above header to the request
+			.pause(1 seconds, 5 seconds) //random pause 1-5 seconds
 		.exec(http("expecting error")
 			.get("/nimrod.php?error=1")
-			.check(status.is(500))
+			.check(status.is(500)) // expecting 500 response the defualt is 200 range
 			.headers(headers_0))
 
 	val otherScn = scenario("other scenario")
@@ -34,5 +34,7 @@ class Simple2 extends Simulation {
 			.headers(headers_0))
 
 
-	setUp(scn.inject(atOnceUsers(10)), otherScn.inject(rampUsersPerSec(100) to(500) during(1 minutes) randomized)).protocols(httpProtocol)
+	setUp(scn.inject(atOnceUsers(10)),  //first scenario will be all 10 users at once
+	      otherScn.inject(rampUsersPerSec(100) to(500) during(1 minutes) randomized)). //start with 100 and up to 500 in 1 minute
+	      protocols(httpProtocol)
 }
