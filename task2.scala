cd88dcd352f7105b6ee694164ffbe1cf83fc825d
Nimrod Lahav <nimrodl@wix.com>
2015-04-16 09:24:54 +0300

    Update task2.scala

--- task2.scala
+++ task2.scala
@@ -16,25 +16,35 @@ class Simple2 extends Simulation {
 		.acceptLanguageHeader("en-US,en;q=0.8,he;q=0.6")
 		.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36")
 
-	val headers_0 = Map("whoAmI" -> "Nimrod") //just to see who this is in logs/relic
+	val headers_0 = Map("whoAmI" -> "Nimrod")
 
-	val scn = scenario("scenario")
+	val random = new util.Random
+	val emailsAndPasswordFeed = Iterator.continually(Map("email" -> s"${random.nextString(20)}@foo.com", "pass" -> random.nextString(10)))
+
+	val scn = scenario("Home Page visitor")
 		.exec(http("get home page")
 			.get("/nimrod.php")
-			.headers(headers_0)) //adds the above header to the request
-			.pause(1 seconds, 5 seconds) //random pause 1-5 seconds
+			.headers(headers_0))
+			.pause(1 seconds, 5 seconds) //pasue a random time 
 		.exec(http("expecting error")
 			.get("/nimrod.php?error=1")
-			.check(status.is(500)) // expecting 500 response the defualt is 200 range
+			.check(status.is(500)) //expecting to get 500 response 
 			.headers(headers_0))
 
-	val otherScn = scenario("other scenario")
-		.exec(http("get home page")
-			.get("/nimrod.php")
-			.headers(headers_0))
+	val otherScn = scenario("User register/login Flow")
+				.feed(emailsAndPasswordFeed) //using a feeder to generate random email/users
+				.exec(http("register")
+					.post("/nimrod.php")
+					.headers(headers_0)
+					.formParam("email", "${email}")
+					.formParam("password", "${pass}"))
+					.pause(1 seconds, 10 seconds)
+				.exec(http("login")
+					.post("/nimrod.php")
+					.headers(headers_0)
+					.formParam("loginemail", "${email}")
+					.formParam("loginpassword", "${pass}"))
 
 
-	setUp(scn.inject(atOnceUsers(10)),  //first scenario will be all 10 users at once
-	      otherScn.inject(rampUsersPerSec(100) to(500) during(1 minutes) randomized)). //start with 100 and up to 500 in 1 minute
-	      protocols(httpProtocol)
+	setUp(scn.inject(atOnceUsers(20)), otherScn.inject(rampUsersPerSec(10) to(50) during(3 minutes) randomized)).protocols(httpProtocol)
 }
